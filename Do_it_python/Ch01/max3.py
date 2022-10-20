3d8f9c7a6b7e2d5a085adaf6e055f87fda0a5ede
Jaemin-Mac <jaemin.ko98@gmail.com>
2022-10-20 23:57:01 +0900

    Do it python data structure

---
+++ Do_it_python/Ch01/max3.py
@@ -0,0 +1,11 @@
+print('세 정수의 최댓값')
+
+a = int(input('정수 a'))
+b = int(input('정수 b'))
+c = int(input('정수 c'))
+
+maximun = a
+if b > maximun: maximum = b
+if c > maximum: maximum = c
+
+print(f'최댓값은 {maximum}입니다.')
