2038000cad4cb36a7800279b78f1f0ae3fe9110f
Jaemin-Mac <jaemin.ko98@gmail.com>
2022-11-03 00:16:13 +0900

    median3.py 27p

---
+++ Do_it_python/Ch01/median3.py
@@ -0,0 +1,23 @@
+#세 정수를 입력받아 중앙값 구하기 1 page 24
+
+def med3(a, b, c):
+	if a>=b:
+		if b>=c:
+			return b
+		elif a <= c:
+			return a
+		else:
+			return c
+	elif a > c:
+		return a
+	elif b > c:
+		return c
+	else:
+		return b
+
+print("세 정수의 중앙값을 구합니다.")
+a = int(input('integer a: '))
+b = int(input('integer b: '))
+c = int(input('integer c: '))
+
+printf(f'중앙값은 {ned3(a,b,c)}입니다.')
